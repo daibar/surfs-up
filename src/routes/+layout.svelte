55fbe285082b07128d43e9bc3ae255dfd002c505
daibar <dannyaibar@gmail.com>
2023-11-07 00:44:03 -0500

    Getting things set up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Sidebar,
+		SidebarBrand,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper,
+		Drawer,
+		CloseButton,
+		SidebarDropdownWrapper,
+		Button,
+		SidebarDropdownItem
+	} from 'flowbite-svelte';
+	import { sineIn } from 'svelte/easing';
+	let transitionParams = {
+		x: -320,
+		duration: 200,
+		easing: sineIn
+	};
+	let width: number;
+	let backdrop: boolean = false;
+	let activateClickOutside = true;
+	onMount(() => {});
+	let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
+	let darkmodebtn =
+		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-2 top-12  md:top-3 md:right-2 z-50';
+	let divClass = 'w-full md:block md:w-auto pr-8';
+	let ulClass = 'flex flex-col p-4 mt-4 md:flex-row-reverse md:space-x-8 md:mt-0 md:text-lg md:font-medium';
+</script>
+
+<header>
+	<Navbar let:hidden>
+		<NavUl {hidden} {divClass} {ulClass}>
+			<NavLi href="https://github.com/daibar/surfs-up">GitHub</NavLi>
+		</NavUl>
+	</Navbar>
+	<DarkMode btnClass={darkmodebtn} />
+</header>
+
+<div class="flex px-4 mx-auto w-full dark:bg-gray-800">
+	<main class="lg:ml-72 w-full mx-auto">
+		<slot />
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Chart, Card, A, Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+
+  let options = {
+    chart: {
+      height: '400px',
+      maxWidth: '100%',
+      type: 'area',
+      fontFamily: 'Inter, sans-serif',
+      dropShadow: {
+        enabled: false
+      },
+      toolbar: {
+        show: false
+      }
+    },
+    tooltip: {
+      enabled: true,
+      x: {
+        show: false
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        opacityFrom: 0.55,
+        opacityTo: 0,
+        shade: '#1C64F2',
+        gradientToColors: ['#1C64F2']
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      width: 6
+    },
+    grid: {
+      show: false,
+      strokeDashArray: 4,
+      padding: {
+        left: 2,
+        right: 2,
+        top: 0
+      }
+    },
+    series: [
+      {
+        name: 'New users',
+        data: [{x: 1, y: 6500},{x: 2, y:6418}, {x: 3, y: 6456},{x: 4, y: 6526},{x: 6356, y: 6456}],
+        color: '#1A56DB'
+      }
+    ],
+    xaxis: {
+      labels: {
+        show: false
+      },
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      }
+    },
+    yaxis: {
+      show: true
+    }
+  };
+</script>
+
+<Card>
+  <div class="flex justify-between">
+    <div>
+      <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">Swell Height</h5>
+    </div>
+ </div>
+  <Chart {options} />
+  <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
+    <div class="flex justify-between items-center pt-5">
+      <Button class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white bg-transparent hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent focus:ring-transparent dark:focus:ring-transparent py-0">Last 7 days</Button>
+  </div>
+  </div>
+</Card>
+
