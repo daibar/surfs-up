e3cde00950c8c00e6adaac316bd4bf7649f4b949
daibar <dannyaibar@gmail.com>
2023-11-17 00:43:20 -0500

    Fix seed command

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,29 @@
 import { PrismaClient } from '@prisma/client'
+import { create } from 'domain'
 const prisma = new PrismaClient()
 async function main() {
-  const horseshoes = await prisma.spot.create({
-    data: {
+  console.log('Start seeding...')
+  const horseshoes = await prisma.spot.upsert({
+    where: {name: 'Horseshoes'},
+    update: {},
+    create: {
         name: 'Horseshoes',
         latitude: 32.8391177,
         longitude: -117.2847756,
     }
  })
- console.log({ horseshoes })
+
+  const scripps = await prisma.spot.upsert({
+    where: {name: 'Scripps'},
+    update: {},
+    create: {
+        name: 'Scripps',
+        latitude: 32.8656048,
+        longitude: -117.256843
+    }
+ })
+
+ console.log({horseshoes, scripps})
 }
 
 main()
